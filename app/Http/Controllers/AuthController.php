b774fb29ac654ffa459d6482cc644cce2fe85939
Alexandre Velasques <alexandrevelasques@hotmail.com>
2026-03-23 01:23:10 -0300

    basic authentication and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,11 +13,6 @@ class AuthController extends Controller
         return view('login');
     }
 
-    public function logout()
-    {
-        echo 'logout';
-    }
-
     public function loginSubmit(Request $request)
     {
         //form validation
@@ -38,11 +33,34 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        $userModel = new User();
-        $users = $userModel->all()->toArray();
+        $user = User::where('username', $username)->where('deleted_at', NULL)->first();
 
-        echo "<pre>";
-        print_r($users);
+        if (!$user) {
+            return redirect()->back()->withInput()->with('loginError', 'Username ou Password incorretos');
+        }
+
+        if (!password_verify($password, $user->password)) {
+            return redirect()->back()->withInput()->with('loginError', 'Username ou Password incorretos');
+        }
+
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username,
+            ]
+        ]);
+
+        echo 'LOGIN COM SUCESSO';
+
+    }
+
+    public function logout()
+    {
+        session()->forget('user');
+        return redirect()->to('/login');
     }
 
 }
