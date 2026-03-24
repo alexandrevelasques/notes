9fd2f113eb9a9d25959b2b6b99dfea143ee1587a
Alexandre Velasques <alexandrevelasques@hotmail.com>
2026-03-24 17:21:14 -0300

    add and edit notes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -42,14 +43,59 @@ class MainController extends Controller
 
             ]
         );
+
+        $id = session('user.id');
+
+        $note = New Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        return redirect()->route('home');
+
+
     }
 
     public function editNote($id)
     {
 
         $id = Operations::decryptId($id);
-        echo 'editNote';
+        $note  = Note::find($id);
 
+        return view('edit_note', ['note' => $note]);
+
+    }
+
+    public function editNoteSubmit(Request $request){
+        $request->validate(
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            [
+                'text_title.required' => 'O título é obrigatorio',
+                'text_title.min' => 'O título deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo:max caracteres',
+                'text_note.required' => 'A nota é obrigatória',
+                'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres'
+
+            ]
+        );
+
+        if ($request->note_id == null) {
+            return redirect()->route('home');
+        }
+
+        $id = Operations::decryptId($request->note_id);
+
+        $note = Note::find($id);
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        return redirect()->route('home');
     }
 
     public function deleteNote($id)
